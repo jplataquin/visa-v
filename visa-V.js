6fd1284daf64bc2cdcfab788dad8992dd6103a81
John Patrick Lataquin <jp.lataquin@gmail.com>
2020-04-05 17:06:40 +0800

    Fixed clone() bug and added debug feature

--- visa-V.js
+++ visa-V.js
@@ -9,6 +9,10 @@
  */
 (function(){
 
+    window.vVconfig = {
+      debug:0
+    };
+
     //Initalize script eval function for layout.
     //Use crazy fonts to make it hard for user code to access them.
     let iframe_eval = function(ć̴̡̢̤̣̠͖̦̜́͆̍̈́͐̊͗ớ̴̼̙̜̦̣̈́͌̏̾m̷̢̢̡̭͉̥̮̤͗̄̓̈̋̈́̈̓̈́̕m̷̨̖̥͚͍͔̓̿̍̀a̴͓̼̟̗̤͕͛̌̃͌̂͜n̷̯̒̌̎̎̀̈́́̓̓̓d̶͈̟̥̗̈́̿̈́̎́,$doc,$param,ȇ̸͕̪̱̟̱̫͆̄͋̈́ͅͅl̵̹͎̱̲̤͐̓̄̿̓̇͒s̶̺͕̬͖̎͊̀͂͑̿͗͠){
@@ -73,6 +77,9 @@
 
         //append iframe to body so it will be initialized
         document.body.append(iframe);
+        if(window.vVconfig.debug == 1){
+          console.log("vV: iFrame url:"+iframe.src+" attached to body");
+        }
 
         //Set up a promise for when the view is ready it will resolve, attach to container for easy access later on
         container.promise =  new Promise((resolve)=>{
@@ -81,14 +88,30 @@
             });
         });
 
+
         //Set up on ready promise interface for user
         container.ready = function(){
-            return container.promise;
+          if(window.vVconfig.debug == 1){
+            console.log("vV: view "+id+" is ready");
+          }
+          return container.promise;
         }
 
         //Set up clone method for view
         container.clone = function(){
-          return this.cloneNode( true );
+
+          let container  = document.createElement('div');
+          let script     = this.cloneData.script;
+          let template   = this.cloneData.template;
+          let params     = this.cloneData.params;
+
+          container.innerHTML = template;
+
+          var els = container.querySelectorAll('[data-el]');
+               
+          iframe_eval(script,container,params,els);
+          
+          return container;
         }
 
         //Initialize number of scoped style element found
@@ -171,12 +194,24 @@
               e.origin != window.location.origin &&  
               location.hostname != 'localhost'
           ){
+
+              if(window.vVconfig.debug == 1){
+                console.log('vV: Post message blocked, protocol or origin unauthorized');
+                console.log(e);
+              }
+
               return false;          
           }else if(
                window.location.protocol == 'file:' && 
                e.origin != 'null' && 
                e.origin != 'file://'
           ){
+
+             if(window.vVconfig.debug == 1){
+                console.log('vV: Post message blocked, protocol or origin unauthorized');
+                console.log(e);
+              }
+              
               return false;
           }
 
@@ -184,6 +219,12 @@
           try{
               var message = JSON.parse(e.data);
           }catch(e){
+
+              if(window.vVconfig.debug == 1){
+                console.log("vV: Post message can't be parsed by json");
+                console.log(e.data);
+              }
+              
               return false;//Ignore message
           }
 
@@ -193,12 +234,26 @@
              typeof message['template'] == 'undefined' ||
              typeof message['script']   == 'undefined' 
           ){
+
+              if(window.vVconfig.debug == 1){
+                console.log("vV: Incomplete message content");
+                console.log(message);
+              }
+
               return false;//Ignore message
           }
 
           //Check if this hook is a type view
-          if(!message.hookType != 'view') return false;
-          
+          if(message.hookType != 'view') {
+            
+            if(window.vVconfig.debug == 1){
+              console.log("vV: hookType is not valid");
+              console.log(message.hookType);
+            }
+
+            return false;
+          }
+
           //Initialize variables from id
           let container     = internal_scope_registry_of_views[message.id].container;
           let iframe        = internal_scope_registry_of_views[message.id].iframe;
@@ -207,6 +262,11 @@
           //Get template and script from message
           let template      = message.template
           let script        = message.script;
+
+          container.cloneData           = {};
+          container.cloneData.template  = template;
+          container.cloneData.script    = script;
+          container.cloneData.params    = params;
 
           //Render template to container
           container.innerHTML = template;
